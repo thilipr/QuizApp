9f9428f51dbaf87b1db998e75a21b66b21dfe515
Thilini Pramoda <42500402+thilipr@users.noreply.github.com>
2020-11-11 23:54:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,35 @@ availableQuestions.push(quiz[i]);
 }
 }
 
+const quiz = [
+    {
+        q:'The behaviour of the instances present of a class inside a method is defined by __________',
+        options: ['Method','Classes','Interfaces','Classes and Interfaces'],
+        answer:1
+    },
+    {
+        q:'The keyword or the property that you use to refer to an object through which they were invoked is _________',
+        options: ['from','to','this','object'],
+        answer:2
+    },
+    {
+        q:'What will be the output of the following JavaScript code?',
+        options: ['false','true','0','1'],
+        answer:1
+    },
+    {
+        q:'The basic difference between JavaScript and Java is _________',
+        options: ['There is no difference','Functions are considered as fields','Variables are specific','Functions are values, and there is no hard distinction between methods'],
+        answer:3
+    },
+    {
+        q:'The meaning for Augmenting classes is that ___________',
+        options: ['objects inherit prototype properties even in a dynamic state','objects inherit prototype properties only in a dynamic state','objects inherit prototype properties in the static state','object doesn’t inherit prototype properties in the static state'],
+        answer:0
+    }   
+]
+
+
 //set question number
 //get random question
 
